0fffebb9504e0d183c34ad423811e444756be81c
Robert Theis <robert.m.theis@gmail.com>
2022-09-08 12:21:45 -0500

    Adjust return type

--- ZeitSatTrackLib/TwoLineElementSet.swift
+++ ZeitSatTrackLib/TwoLineElementSet.swift
@@ -23,7 +23,7 @@ extension String
         let start = self.index(self.startIndex, offsetBy: r.lowerBound)
         let end = self.index(self.startIndex, offsetBy: r.upperBound)
         
-        return self[start...end]
+        return String(self[start...end])
     }
 }
 
